4b3381792e81a1bbbe3deb4f1b3a962b6dd4aa23
MatejTuray <41123694+MatejTuray@users.noreply.github.com>
2018-09-09 21:42:06 +0200

    heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 mongoose.Promise = global.Promise
 
-mongoose.connect("mongodb://localhost:27017/Movies", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/Movies", { useNewUrlParser: true })
 module.exports = { mongoose };
