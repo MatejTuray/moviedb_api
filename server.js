3c166630ff03eec2ee4a9c8f0556e2972f08616e
MatejTuray <41123694+MatejTuray@users.noreply.github.com>
2018-09-09 22:05:36 +0200

    testDB

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const env = process.env.NODE_ENV || "developement";
+if (env === "developement") {
+    process.env.PORT = 5000;
+    process.env.MONGODB_URI = "mongodb://localhost:27017/Movies"
+}
+else if (env === "test") {
+    process.env.PORT = 5000;
+    process.env.MONGODB_URI = "mongodb://localhost:27017/test"
+}
+console.log("env:", env)
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+
 
 
 const port = process.env.PORT || 5000;
