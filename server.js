bbb568c7614292ea287aaf97ea8fc89576941c83
MatejTuray <41123694+MatejTuray@users.noreply.github.com>
2018-09-11 20:21:18 +0200

    helm

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ console.log("env:", env)
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const helmet = require("helmet")
 
 
 
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
+app.use(helmet())
 app.use(express.urlencoded({ extended: true }));
 require("./routes/routes")(app)
 app.get("*", (req, res) => {
